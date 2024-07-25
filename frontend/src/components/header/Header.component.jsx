c24c53587e923a84f3cb29e1b8f9b53d49f80d2d
Anastasiia <anastasiia.khrypko@dci-student.org>
2024-07-25 13:25:45 +0200

    header component is added

---
+++ frontend/src/components/header/Header.component.jsx
@@ -0,0 +1,11 @@
+function HeaderComponent() {
+
+    return (
+        <header>
+          <h1>MindPad<span>*</span></h1>
+          <p>(* write down something that cannot be forgotten)</p>
+        </header>
+    )
+  }
+  
+  export default HeaderComponent;
